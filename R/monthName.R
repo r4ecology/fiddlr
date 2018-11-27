3d4de3aa314f9c9579f2355db72bf00402538804
Robert Wilson <robertwilson190@gmail.com>
2018-11-27 09:55:41 +0000

    give a more sensible name to the function that converts the number of a month to the name of a month

--- R/monthName.R
+++ R/monthName.R
@@ -9,7 +9,7 @@
 
 
 
-monthName <- function (mm) {
+month_name <- function (mm) {
 	mm <- as.integer(mm)
 	months <- return(c("January", "February", "March", "April", "May", "June", "July", "August", "September", "October", "November", "December")[mm])
 
@@ -24,40 +24,4 @@ monthName <- function (mm) {
 #' @return Nearest value to x in vector Y
 #'
 #' @export
-
-
-
-nearestValue <- function (x, Y) {
-	return(Y[which(abs(Y - x) == min(abs(Y - x)))[1]])
-}
-
-
-
-
-#' Bin a value within a vector
-#'
-#' Takes a number and works out where it lies in another vector
-#'
-#' @param x A number
-#' @return Nearest value to x in vector Y
-#'
-#' @export
-
-
-
-binValue <- function (X, Y, method = "lower") {
-
-	binned <- function (x, Y, method = "lower") {
-
-  	if(method == "lower")
-  		return( Y[ ifelse(Y[which(abs(Y - x) == min(abs(Y - x)))[1]] > x, -1,0) + which(abs(Y - x) == min(abs(Y - x)))[1]   ])
-
-  	if(method == "upper")
-  		return( Y[ ifelse(Y[which(abs(Y - x) == min(abs(Y - x)))[1]] < x, 1,0) + which(abs(Y - x) == min(abs(Y - x)))[1]   ])
-
-	}
-
-		return(apply(as.matrix(X), MARGIN = 1, FUN = binned, Y = Y))
-}
-
 
