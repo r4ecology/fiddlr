d6256c8f4b9822ce8908d167053092b2d2f680b4
Robert Wilson <robertwilson190@gmail.com>
2018-11-27 09:54:44 +0000

    give a more sensible name to the function that extracts a number from a string

--- R/extNumFromString.R
+++ R/extNumFromString.R
@@ -9,25 +9,8 @@
 
 
 
-extNumFromString <- function (x) {
+num_from_string <- function (x) {
 
 return(unique(na.omit(as.numeric(unlist(strsplit(unlist(x), "[^0-9]+"))))))
 }
 
-
-#' Create a vector of days of the year from a data frame
-#'
-#' Takes a string and extracts the numbers that appear in it. Useful for cases where you want to pull out years
-#'
-#' @param df. A data frame which has Year, Month and Day in named columns
-#' @return A vector with the days of the year in each row
-#'
-#' @export
-
-
-doyFromDF <- function (df, yy) {
-	if(missing(yy))
-      return(lubridate::yday(lubridate::ymd(paste0(df$Year, "-", df$Month, "-", df$Day)))) else
-    return(lubridate::yday(lubridate::ymd(paste0(yy, "-", df$Month, "-", df$Day))))
-
-}
